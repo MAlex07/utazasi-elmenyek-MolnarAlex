b705500169599f56a361e56762635987aaeb2c97
Petrik Diak <diak@petrik.hu>
2026-03-04 11:19:52 +0100

    kész van a class

---
+++ src/elmeny.ts
@@ -0,0 +1,11 @@
+export class Elmeny{
+    place: string;
+    story: string;
+    season: string;
+
+    constructor(place: string, story: string, season: string){
+        this.place = place;
+        this.story = story;
+        this.season =  season;
+    }
+}
